0c57d1d9017c0e5d2ee900008b59926cf48ddba2
Shlikhutka.Bohdan <bohdan.shlikhutka@gmail.com>
2022-02-17 19:01:09 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ app.get('/balance', async (req, res) => {
     });
 })
 
+app.get('/health-check', async (req, res) => {
+    res.send({
+        "balance": "0"
+    });
+})
+
 app.get('/nft', async (req, res) => {
     res.send({
         "message": "Hello world!"
