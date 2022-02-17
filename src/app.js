243a8d399d19047883e3b036f26c25ba850d8446
Shlikhutka.Bohdan <bohdan.shlikhutka@gmail.com>
2022-02-17 17:54:43 +0200

    express integrate

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/balance', async (req, res) => {
+    res.send({
+        "balance": "0"
+    });
+})
+
+app.get('/nft', async (req, res) => {
+    res.send({
+        "message": "Hello world!"
+    });
+})
+
+app.listen(80, () => {
+    console.log('HTTP server running on port 80');
+});
