bea31d9c434974dfc2d6d8ba3a66672a49321f82
Shlikhutka.Bohdan <bohdan.shlikhutka@gmail.com>
2022-02-18 16:20:05 +0200

    fix

--- src/cardano-gateway.js
+++ src/cardano-gateway.js
@@ -20,92 +20,96 @@ const CardanoGateway = function () {
     }
 
     this.mintNft = async function(params) {
-        const { receivers, metadata, tokenName } = params
+        try {
+            const { receivers, metadata, tokenName } = params
 
-        // 2. Define mint script
+            // 2. Define mint script
 
-        const queryTip = cardano.queryTip();
-        console.log(queryTip);
+            const queryTip = cardano.queryTip();
+            console.log(queryTip);
 
-        const currentSlot = queryTip.slot;
-        const slotNumber = currentSlot + 10000;
-        console.log("Slot number :", slotNumber);
+            const currentSlot = queryTip.slot;
+            const slotNumber = currentSlot + 10000;
+            console.log("Slot number :", slotNumber);
 
-        const policyScript = {
-            keyHash: cardano.addressKeyHash(policyAccount.name),
-            type: "sig"
-        }
-        console.log("policyScript: ", JSON.stringify(policyScript))
+            const policyScript = {
+                keyHash: cardano.addressKeyHash(policyAccount.name),
+                type: "sig"
+            }
+            console.log("policyScript: ", JSON.stringify(policyScript))
 
-        // 3. Create POLICY_ID
-        const POLICY_ID = cardano.transactionPolicyid(policyScript)
-        console.log("POLICY_ID: ", POLICY_ID)
+            // 3. Create POLICY_ID
+            const POLICY_ID = cardano.transactionPolicyid(policyScript)
+            console.log("POLICY_ID: ", POLICY_ID)
 
-        // 4. Define ASSET_NAME
-        const ASSET_NAME = tokenName;
-        const ASSET_NAME_HEX = Buffer.from(ASSET_NAME, 'utf8').toString('hex');
-        const ASSET_ID = POLICY_ID + "." + ASSET_NAME_HEX
-        console.log("ASSET_ID: ", ASSET_ID)
+            // 4. Define ASSET_NAME
+            const ASSET_NAME = tokenName;
+            const ASSET_NAME_HEX = Buffer.from(ASSET_NAME, 'utf8').toString('hex');
+            const ASSET_ID = POLICY_ID + "." + ASSET_NAME_HEX
+            console.log("ASSET_ID: ", ASSET_ID)
 
-        // 6. Define metadata
-        const fullMetadata = {
-            721: {
-                [POLICY_ID]: {
-                    [ASSET_NAME]: {
-                        ...metadata
+            // 6. Define metadata
+            const fullMetadata = {
+                721: {
+                    [POLICY_ID]: {
+                        [ASSET_NAME]: {
+                            ...metadata
+                        }
                     }
                 }
             }
-        }
-        console.log("metadata: ", JSON.stringify(metadata))
+            console.log("metadata: ", JSON.stringify(metadata))
 
-        // 7. Define transaction
-        const minimumMintTxOut = {
-            lovelace: config.minimumMintTxOut
-        }
-
-        const oldBalance = cleanObject(paymentAccount.balance().value)
-        console.log("oldBalance: ", oldBalance)
-
-        const receiversCount = receivers.length
-        console.log("receiversCount: ", receiversCount)
-
-        const changeBalance = {
-            ...oldBalance,
-            lovelace: oldBalance.lovelace - receiversCount * minimumMintTxOut.lovelace
-        }
-        console.log("changeBalance: ", changeBalance)
-
-        const cleanedTxIn = Object.values(deepCleanObject(paymentAccount.balance().utxo))
-        console.log("cleanedTxIn: ", cleanedTxIn)
-
-        const receiversTxOut = receivers.map(receiver => {
-            return {
-                address: receiver,
-                value: { ...minimumMintTxOut, [ASSET_ID]: 1 }
+            // 7. Define transaction
+            const minimumMintTxOut = {
+                lovelace: config.minimumMintTxOut
             }
-        });
 
-        const tx = {
-            txIn: cleanedTxIn,
-            txOut: [
-                {
-                    address: paymentAccount.paymentAddr,
-                    value: { ...changeBalance }
+            const oldBalance = cleanObject(paymentAccount.balance().value)
+            console.log("oldBalance: ", oldBalance)
+
+            const receiversCount = receivers.length
+            console.log("receiversCount: ", receiversCount)
+
+            const changeBalance = {
+                ...oldBalance,
+                lovelace: oldBalance.lovelace - receiversCount * minimumMintTxOut.lovelace
+            }
+            console.log("changeBalance: ", changeBalance)
+
+            const cleanedTxIn = Object.values(deepCleanObject(paymentAccount.balance().utxo))
+            console.log("cleanedTxIn: ", cleanedTxIn)
+
+            const receiversTxOut = receivers.map(receiver => {
+                return {
+                    address: receiver,
+                    value: { ...minimumMintTxOut, [ASSET_ID]: 1 }
+                }
+            });
+
+            const tx = {
+                txIn: cleanedTxIn,
+                txOut: [
+                    {
+                        address: paymentAccount.paymentAddr,
+                        value: { ...changeBalance }
+                    },
+                    ...receiversTxOut
+                ],
+                mint: {
+                    action: [{ type: "mint", quantity: 2, asset: ASSET_ID }],
+                    script: [policyScript]
                 },
-                ...receiversTxOut
-            ],
-            mint: {
-                action: [{ type: "mint", quantity: 2, asset: ASSET_ID }],
-                script: [policyScript]
-            },
-            fullMetadata,
-            witnessCount: 2
-        }
-        console.log("tx: ", JSON.stringify(tx))
+                fullMetadata,
+                witnessCount: 2
+            }
+            console.log("tx: ", JSON.stringify(tx))
 
-        return {
-            ...tx
+            return {
+                ...tx
+            }
+        } catch (e) {
+            console.log(e)
         }
     }
 
